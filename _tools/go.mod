18b06da9678819dcb7a01ab298fd2f20d4cdde6c
Aleksandr Razumov <ernado@go-faster.org>
2023-09-24 18:19:51 +0300

    feat: generate enums

---
+++ _tools/go.mod
@@ -0,0 +1,8 @@
+module github.com/gotd/td/_tools
+
+go 1.16
+
+require (
+	github.com/dmarkham/enumer v1.5.8
+	golang.org/x/tools v0.13.0 // indirect
+)
--- schedpolicy.go
+++ schedpolicy.go
@@ -1,37 +1,18 @@
 package schedpolicy
 
-import "fmt"
+//go:generate go run -modfile=_tools/go.mod github.com/dmarkham/enumer -type Policy -transform lower -text -output policy_gen.go
 
 // Policy is a scheduling policy.
 type Policy int
-
-func (p Policy) String() string {
-	switch p {
-	case Normal:
-		return "normal"
-	case FIFO:
-		return "fifo"
-	case RR:
-		return "rr"
-	case Batch:
-		return "batch"
-	case Idle:
-		return "idle"
-	case Deadline:
-		return "deadline"
-	default:
-		return fmt.Sprintf("unknown(%d)", int(p))
-	}
-}
 
 // Possible values for sched_policy.
 //
 // https://github.com/torvalds/linux/blob/d90b0276af8f25a0b8ae081a30d1b2a61263393b/include/uapi/linux/sched.h#L111-L120
 const (
-	Normal   = 0
-	FIFO     = 1
-	RR       = 2
-	Batch    = 3
-	Idle     = 5
-	Deadline = 6
+	Normal   Policy = 0
+	FIFO     Policy = 1
+	RR       Policy = 2
+	Batch    Policy = 3
+	Idle     Policy = 5
+	Deadline Policy = 6
 )
